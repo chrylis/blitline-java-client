cb08b90a0a803419bcfbb54905761e88edc3a389
Christopher Smith <chrylis@gmail.com>
2014-03-06 21:00:22 -0600

    clean out unnecessary methods (color handling)

--- blitline-image-client/src/main/java/com/blitline/image/functions/AbstractFunction.java
+++ blitline-image-client/src/main/java/com/blitline/image/functions/AbstractFunction.java
@@ -1,10 +1,8 @@
 package com.blitline.image.functions;
 
+import java.util.Collections;
 import java.util.HashMap;
-import java.util.Locale;
 import java.util.Map;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 import org.apache.commons.lang3.Validate;
 
@@ -19,27 +17,10 @@ import com.blitline.image.Function;
  */
 public abstract class AbstractFunction implements Function {
 
-	public static final Pattern HTML_COLOR_PATTERN = Pattern.compile("\\A#?(\\p{XDigit}{6})\\z");
-
 	protected Map<String, Object> params = new HashMap<String, Object>();
 
 	public Map<String, Object> getParams() {
-		return params;
-	}
-
-	public static String normalizeHtmlColor(String htmlColor) {
-		Matcher m = HTML_COLOR_PATTERN.matcher(htmlColor);
-		if (!m.matches())
-			throw new IllegalArgumentException("the string \"" + htmlColor + "\" is not a valid HTML color");
-
-		return "#" + m.group(1).toLowerCase(Locale.ENGLISH);
-	}
-
-	public static String rgbToHtmlColor(int red, int green, int blue) {
-		if (red > 255 || red < 0 || green > 255 || green < 0 || blue > 255 || blue < 0)
-			throw new IllegalArgumentException("all RGB values must be in the range of 0 to 255");
-
-		return String.format("#%02x%02x%02x", red, green, blue);
+		return Collections.unmodifiableMap(params);
 	}
 
 	protected void putOpacity(double opacity) {
