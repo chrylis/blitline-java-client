0ffdd2380bf7969533f0d87ce24ccd0e8a2c694f
Christopher Smith <chrylis@gmail.com>
2014-05-01 12:01:44 -0500

    add extended-meta option

--- blitline-image-client/src/main/java/com/blitline/image/BlitlineImageJob.java
+++ blitline-image-client/src/main/java/com/blitline/image/BlitlineImageJob.java
@@ -18,66 +18,75 @@ import com.fasterxml.jackson.databind.annotation.JsonNaming;
 @JsonNaming(PropertyNamingStrategy.LowerCaseWithUnderscoresStrategy.class)
 @JsonInclude(Include.NON_EMPTY)
 public class BlitlineImageJob {
-	
+
 	private final String applicationId;
 	private final Object src;
+	private final Boolean extendedMetadata;
 	private final String postbackUrl;
 	private final List<Function> functions = new LinkedList<Function>();
-	
-	public BlitlineImageJob(String applicationId, Object src, String postbackUrl) {
+
+	public BlitlineImageJob(String applicationId, Object src, Boolean extendedMetadata, String postbackUrl) {
 		Validate.notNull(applicationId, "application ID must not be null");
 		this.applicationId = applicationId;
-		
+
 		Validate.notNull(src,"image source must not be null");
 		this.src = src;
-		
+
+		this.extendedMetadata = extendedMetadata;
+
 		this.postbackUrl = postbackUrl;
 	}
-	
+
 	public String getApplicationId() {
 		return applicationId;
 	}
-	
+
 	public Object getSrc() {
 		return src;
 	}
-	
+
+	public Boolean getExtendedMetadata() {
+		return extendedMetadata;
+	}
+
 	public String getPostbackUrl() {
 		return postbackUrl;
 	}
-	
+
 	public String getV() {
 		return Blitline.BLITLINE_API_VERSION;
 	}
-	
+
 	public List<Function> getFunctions() {
 		return Collections.unmodifiableList(functions);
 	}
-	
+
 	public void apply(Function... functions) {
 		this.functions.addAll(Arrays.asList(functions));
 	}
-	
+
 	public static Builder forApplication(String applicationId) {
 		return new Builder(applicationId);
 	}
 
 	/**
 	 * Fluent builder class for a {@link BlitlineImageJob} instance.
-	 * 
+	 *
 	 * @author Christopher Smith
-	 * 
+	 *
 	 */
 	public static class Builder {
 		private final String applicationId;
 
 		private Object src;
 
+		private Boolean extendedMetadata;
+
 		private String postbackUrl;
 
 		/**
 		 * Constructs a {@code Builder} instance for a single {@code Job}.
-		 * 
+		 *
 		 * @param applicationId
 		 *            your Blitline application ID key
 		 */
@@ -87,7 +96,7 @@ public class BlitlineImageJob {
 
 		/**
 		 * Convenience method that simply calls the standard constructor.
-		 * 
+		 *
 		 * @param applicationId
 		 *            your Blitline application ID key
 		 * @return a new {@code Builder} instance that will build a {@code Job} for the provided key
@@ -99,14 +108,14 @@ public class BlitlineImageJob {
 		private void setSrc(Object src) {
 			if (this.src != null)
 				throw new IllegalStateException("src is already set");
-			
+
 			this.src = src;
 		}
 
 		/**
 		 * Loads the image to be processed from the specified URL. The contents of the string are not encoded and are passed to the
 		 * API as-is.
-		 * 
+		 *
 		 * @param src
 		 *            the URL for the image to be processed
 		 * @return this {@code Builder} object
@@ -119,7 +128,7 @@ public class BlitlineImageJob {
 		/**
 		 * Loads the image to be processed from the specified URL. The contents of the {@code URL} object are not encoded and are
 		 * passed to the API as-is.
-		 * 
+		 *
 		 * @param src
 		 *            the URL for the image to be processed
 		 * @return this {@code Builder} object
@@ -130,7 +139,7 @@ public class BlitlineImageJob {
 
 		/**
 		 * Loads the image to be processed from S3.
-		 * 
+		 *
 		 * @param src
 		 *            the S3 location to load the image from
 		 * @return this {@code Builder} object
@@ -142,7 +151,7 @@ public class BlitlineImageJob {
 
 		/**
 		 * Loads the image to be processed from S3.
-		 * 
+		 *
 		 * @param bucket
 		 *            the ID of the S3 bucket where the image is stored
 		 * @param key
@@ -155,7 +164,7 @@ public class BlitlineImageJob {
 
 		/**
 		 * Specifies an optional callback URL that will receive the notification of job completion or failure.
-		 * 
+		 *
 		 * @param postbackUrl
 		 *            the callback URL
 		 * @return this {@code Builder} object
@@ -167,7 +176,7 @@ public class BlitlineImageJob {
 
 		/**
 		 * Specifies an optional callback URL that will receive the notification of job completion or failure.
-		 * 
+		 *
 		 * @param postbackUrl
 		 *            the callback URL
 		 * @return this {@code Builder} object
@@ -175,9 +184,18 @@ public class BlitlineImageJob {
 		public Builder withPostback(URI postbackUrl) {
 			return withPostback(postbackUrl.toString());
 		}
-		
+
+		/**
+		 * Specifies that this job should return extended metadata such as file size.
+		 * @return this {@code Builder} object
+		 */
+		public Builder withExtendedMetadata() {
+			extendedMetadata = Boolean.TRUE;
+			return this;
+		}
+
 		public BlitlineImageJob apply(Function... functions) {
-			BlitlineImageJob job = new BlitlineImageJob(applicationId, src, postbackUrl);
+			BlitlineImageJob job = new BlitlineImageJob(applicationId, src, extendedMetadata, postbackUrl);
 			job.apply(functions);
 			return job;
 		}
