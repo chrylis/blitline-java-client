60ec8fd7d8c515f148aa964b6cb8d9072ba32e94
Christopher Smith <chrylis@gmail.com>
2014-05-07 03:40:57 -0500

    add service option for extended metadata on all jobs

--- blitline-image-client-spring/src/main/java/com/blitline/image/spring/BlitlineImageService.java
+++ blitline-image-client-spring/src/main/java/com/blitline/image/spring/BlitlineImageService.java
@@ -17,14 +17,14 @@ import com.blitline.image.BlitlinePostResults;
  * set in order to create job specifications and will automatically apply a postback URL for job completion notification if present.
  * It can also optionally remember an S3 source bucket and hold a Spring {@link RestTemplate} configured for compatibility with
  * Blitline's API.
- * 
+ *
  * The Java interface for this class has been designed to work with but has not been tested on Spring for Android's
  * {@code RestTemplate} implementation. The serialization formatting applied to conform to Blitline's syntax is configured using
  * Jackson annotations and is not expected to work as-is with Gson.
- * 
+ *
  * A typical use of this facade after setting the {@code applicationId}, {@code s3bucket}, and optionally {@code postbackUrl} would
  * be:
- * 
+ *
  * <pre>
  * BlitlineImageJob j = bis.loadS3Key(&quot;sourceimg.jpg&quot;).apply(
  * 	Blitline.resizeToFit(512, 384).andSaveResultTo(
@@ -33,12 +33,12 @@ import com.blitline.image.BlitlinePostResults;
  * 			Blitline.toGrayScale().andSaveResultTo(
  * 				SavedImage.withId(&quot;abcd1234.gray&quot;).toS3(&quot;destination-bucket&quot;, &quot;dest-gray.jpg&quot;)
  * 				)));
- * 
+ *
  * ResponseEntity&lt;BlitlinePostResults&gt; resp = bis.submitJob(j);
  * </pre>
- * 
+ *
  * @author Christopher Smith
- * 
+ *
  */
 @Service
 public class BlitlineImageService {
@@ -51,6 +51,8 @@ public class BlitlineImageService {
 
 	@Value("${blitline.s3sourceBucket:#{null}}")
 	private String s3bucket;
+
+	private boolean alwaysExtendedMetadata = true;
 
 	public String getApplicationId() {
 		return applicationId;
@@ -74,6 +76,19 @@ public class BlitlineImageService {
 
 	public void setS3bucket(String s3bucket) {
 		this.s3bucket = s3bucket;
+	}
+
+	public boolean isAlwaysExtendedMetadata() {
+		return alwaysExtendedMetadata;
+	}
+
+	/**
+	 * If this flag is set, this service instance will always request extended image metadata from Blitline.
+	 *
+	 * @param alwaysExtendedMetadata whether to request extended metadata on all jobs
+	 */
+	public void setAlwaysExtendedMetadata(boolean alwaysExtendedMetadata) {
+		this.alwaysExtendedMetadata = alwaysExtendedMetadata;
 	}
 
 	public static final URI BLITLINE_SUBMIT_POST_URI = URI.create("http://api.blitline.com/job");
@@ -108,7 +123,7 @@ public class BlitlineImageService {
 	/**
 	 * Creates a job builder with the application ID set, along with the completion postback URL if specified. The returned builder
 	 * does not have a source location specified.
-	 * 
+	 *
 	 * @return a job builder with application ID and optionally postback URL set
 	 */
 	public BlitlineImageJob.Builder jobBuilder() {
@@ -116,12 +131,15 @@ public class BlitlineImageService {
 		if (postbackUrl != null)
 			builder.withPostback(postbackUrl);
 
+		if(alwaysExtendedMetadata)
+			builder.withExtendedMetadata();
+
 		return builder;
 	}
 
 	/**
 	 * Creates a job builder with the application ID and source URL set, along with the completion postback URL if specified.
-	 * 
+	 *
 	 * @param src
 	 *            the URL of the source image to be loaded
 	 * @return a job builder with application ID and optionally postback URL set
@@ -132,7 +150,7 @@ public class BlitlineImageService {
 
 	/**
 	 * Creates a job builder with the application ID and source URL set, along with the completion postback URL if specified.
-	 * 
+	 *
 	 * @param src
 	 *            the URL of the source image to be loaded
 	 * @return a job builder with application ID and optionally postback URL set
@@ -144,7 +162,7 @@ public class BlitlineImageService {
 	/**
 	 * Creates a job builder with the application ID set, along with the completion postback URL if specified, that will load the
 	 * specified object from the configured S3 source bucket.
-	 * 
+	 *
 	 * @param key
 	 *            the key of the S3 object to be loaded from the configured source bucket as the source image
 	 * @return a job builder with application ID and optionally postback URL set
