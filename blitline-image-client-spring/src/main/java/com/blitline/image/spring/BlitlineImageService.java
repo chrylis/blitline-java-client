754948b261d8a22e2cd7ed28eb27e3c10ced3842
Christopher Smith <chrylis@gmail.com>
2014-11-10 07:00:53 -0600

    0.14.2: add "loadS3Object" method to Spring service

--- blitline-image-client-spring/src/main/java/com/blitline/image/spring/BlitlineImageService.java
+++ blitline-image-client-spring/src/main/java/com/blitline/image/spring/BlitlineImageService.java
@@ -28,12 +28,12 @@ import com.blitline.image.spring.postback.BlitlinePostbackUrlProvider;
  *
  * <pre>
  * BlitlineImageJob j = bis.loadS3Key(&quot;sourceimg.jpg&quot;).apply(
- * 	Blitline.resizeToFit(512, 384).andSaveResultTo(
- * 		SavedImage.withId(&quot;abcd1234.color&quot;).toS3(&quot;destination-bucket&quot;, &quot;dest-color.jpg&quot;)
- * 		).thenApply(
- * 			Blitline.toGrayScale().andSaveResultTo(
- * 				SavedImage.withId(&quot;abcd1234.gray&quot;).toS3(&quot;destination-bucket&quot;, &quot;dest-gray.jpg&quot;)
- * 				)));
+ *     Blitline.resizeToFit(512, 384).andSaveResultTo(
+ *         SavedImage.withId(&quot;abcd1234.color&quot;).toS3(&quot;destination-bucket&quot;, &quot;dest-color.jpg&quot;)
+ *         ).thenApply(
+ *             Blitline.toGrayScale().andSaveResultTo(
+ *                 SavedImage.withId(&quot;abcd1234.gray&quot;).toS3(&quot;destination-bucket&quot;, &quot;dest-gray.jpg&quot;)
+ *                 )));
  *
  * ResponseEntity&lt;BlitlinePostResults&gt; resp = bis.submitJob(j);
  * </pre>
@@ -44,132 +44,146 @@ import com.blitline.image.spring.postback.BlitlinePostbackUrlProvider;
 @Service
 public class BlitlineImageService {
 
-	private String applicationId;
+    private String applicationId;
 
-	public String getApplicationId() {
-		return applicationId;
-	}
+    public String getApplicationId() {
+        return applicationId;
+    }
 
-	@Value("${blitline.applicationId:no application ID set!}")
-	public void setApplicationId(String applicationId) {
-		this.applicationId = applicationId;
-	}
+    @Value("${blitline.applicationId:no application ID set!}")
+    public void setApplicationId(String applicationId) {
+        this.applicationId = applicationId;
+    }
 
-	private BlitlinePostbackUrlProvider postbackUrlProvider;
+    private BlitlinePostbackUrlProvider postbackUrlProvider;
 
-	public BlitlinePostbackUrlProvider getPostbackUrlProvider() {
-		return postbackUrlProvider;
-	}
+    public BlitlinePostbackUrlProvider getPostbackUrlProvider() {
+        return postbackUrlProvider;
+    }
 
-	@Autowired(required = false)
-	public void setPostbackUrlProvider(BlitlinePostbackUrlProvider postbackUrlProvider) {
-		this.postbackUrlProvider = postbackUrlProvider;
-	}
+    @Autowired(required = false)
+    public void setPostbackUrlProvider(BlitlinePostbackUrlProvider postbackUrlProvider) {
+        this.postbackUrlProvider = postbackUrlProvider;
+    }
 
-	private String s3bucket;
+    private String s3bucket;
 
-	public String getS3bucket() {
-		return s3bucket;
-	}
+    public String getS3bucket() {
+        return s3bucket;
+    }
 
-	@Value("${blitline.s3sourceBucket:#{null}}")
-	public void setS3bucket(String s3bucket) {
-		this.s3bucket = s3bucket;
-	}
+    @Value("${blitline.s3sourceBucket:#{null}}")
+    public void setS3bucket(String s3bucket) {
+        this.s3bucket = s3bucket;
+    }
 
-	private boolean alwaysExtendedMetadata = false;
+    private boolean alwaysExtendedMetadata = false;
 
-	public boolean isAlwaysExtendedMetadata() {
-		return alwaysExtendedMetadata;
-	}
+    public boolean isAlwaysExtendedMetadata() {
+        return alwaysExtendedMetadata;
+    }
 
-	/**
-	 * If this flag is set, this service instance will always request extended image metadata from Blitline.
-	 *
-	 * @param alwaysExtendedMetadata
-	 *            whether to request extended metadata on all jobs
-	 */
-	public void setAlwaysExtendedMetadata(boolean alwaysExtendedMetadata) {
-		this.alwaysExtendedMetadata = alwaysExtendedMetadata;
-	}
+    /**
+     * If this flag is set, this service instance will always request extended image metadata from Blitline.
+     *
+     * @param alwaysExtendedMetadata
+     *            whether to request extended metadata on all jobs
+     */
+    public void setAlwaysExtendedMetadata(boolean alwaysExtendedMetadata) {
+        this.alwaysExtendedMetadata = alwaysExtendedMetadata;
+    }
 
-	public static final URI BLITLINE_SUBMIT_POST_URI = URI.create("http://api.blitline.com/job");
+    public static final URI BLITLINE_SUBMIT_POST_URI = URI.create("http://api.blitline.com/job");
 
-	private RestTemplate blitlineRest;
+    private RestTemplate blitlineRest;
 
-	public RestTemplate getBlitlineRest() {
-		return blitlineRest;
-	}
+    public RestTemplate getBlitlineRest() {
+        return blitlineRest;
+    }
 
-	@Autowired(required = false)
-	@BlitlineApi
-	public void setBlitlineRest(RestTemplate blitlineRest) {
-		this.blitlineRest = blitlineRest;
-	}
+    @Autowired(required = false)
+    @BlitlineApi
+    public void setBlitlineRest(RestTemplate blitlineRest) {
+        this.blitlineRest = blitlineRest;
+    }
 
-	public ResponseEntity<BlitlinePostResults> submitJob(BlitlineImageJob job) {
-		return blitlineRest.postForEntity(BLITLINE_SUBMIT_POST_URI, job, BlitlinePostResults.class);
-	}
+    public ResponseEntity<BlitlinePostResults> submitJob(BlitlineImageJob job) {
+        return blitlineRest.postForEntity(BLITLINE_SUBMIT_POST_URI, job, BlitlinePostResults.class);
+    }
 
-	@Override
-	public String toString() {
-		StringBuilder sb = new StringBuilder("BlitlineImageService[applicationId=").append(applicationId);
-		if (postbackUrlProvider != null)
-			sb.append(",postbackUrlProvider=").append(postbackUrlProvider);
-		if (s3bucket != null)
-			sb.append(",s3Bucket=").append(s3bucket);
-		sb.append(']');
-		return sb.toString();
-	}
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder("BlitlineImageService[applicationId=").append(applicationId);
+        if (postbackUrlProvider != null)
+            sb.append(",postbackUrlProvider=").append(postbackUrlProvider);
+        if (s3bucket != null)
+            sb.append(",s3Bucket=").append(s3bucket);
+        sb.append(']');
+        return sb.toString();
+    }
 
-	/**
-	 * Creates a job builder with the application ID set, along with the completion postback URL if specified. The returned builder
-	 * does not have a source location specified.
-	 *
-	 * @return a job builder with application ID and optionally postback URL set
-	 */
-	public BlitlineImageJob.Builder jobBuilder() {
-		BlitlineImageJob.Builder builder = BlitlineImageJob.forApplication(applicationId);
-		if (postbackUrlProvider != null)
-			builder.withPostback(postbackUrlProvider.getPostbackUrl());
+    /**
+     * Creates a job builder with the application ID set, along with the completion postback URL if specified. The returned builder
+     * does not have a source location specified.
+     *
+     * @return a job builder with application ID and optionally postback URL set
+     */
+    public BlitlineImageJob.Builder jobBuilder() {
+        BlitlineImageJob.Builder builder = BlitlineImageJob.forApplication(applicationId);
+        if (postbackUrlProvider != null)
+            builder.withPostback(postbackUrlProvider.getPostbackUrl());
 
-		if (alwaysExtendedMetadata)
-			builder.withExtendedMetadata();
+        if (alwaysExtendedMetadata)
+            builder.withExtendedMetadata();
 
-		return builder;
-	}
+        return builder;
+    }
 
-	/**
-	 * Creates a job builder with the application ID and source URL set, along with the completion postback URL if specified.
-	 *
-	 * @param src
-	 *            the URL of the source image to be loaded
-	 * @return a job builder with application ID and optionally postback URL set
-	 */
-	public BlitlineImageJob.Builder loadUrl(String src) {
-		return jobBuilder().fromUrl(src);
-	}
+    /**
+     * Creates a job builder with the application ID and source URL set, along with the completion postback URL if specified.
+     *
+     * @param src
+     *            the URL of the source image to be loaded
+     * @return a job builder with application ID and optionally postback URL set
+     */
+    public BlitlineImageJob.Builder loadUrl(String src) {
+        return jobBuilder().fromUrl(src);
+    }
 
-	/**
-	 * Creates a job builder with the application ID and source URL set, along with the completion postback URL if specified.
-	 *
-	 * @param src
-	 *            the URL of the source image to be loaded
-	 * @return a job builder with application ID and optionally postback URL set
-	 */
-	public BlitlineImageJob.Builder loadUrl(URI src) {
-		return jobBuilder().fromUrl(src);
-	}
+    /**
+     * Creates a job builder with the application ID and source URL set, along with the completion postback URL if specified.
+     *
+     * @param src
+     *            the URL of the source image to be loaded
+     * @return a job builder with application ID and optionally postback URL set
+     */
+    public BlitlineImageJob.Builder loadUrl(URI src) {
+        return jobBuilder().fromUrl(src);
+    }
 
-	/**
-	 * Creates a job builder with the application ID set, along with the completion postback URL if specified, that will load the
-	 * specified object from the configured S3 source bucket.
-	 *
-	 * @param key
-	 *            the key of the S3 object to be loaded from the configured source bucket as the source image
-	 * @return a job builder with application ID and optionally postback URL set
-	 */
-	public BlitlineImageJob.Builder loadS3Key(String key) {
-		return jobBuilder().fromS3(s3bucket, key);
-	}
+    /**
+     * Creates a job builder with the application ID set, along with the completion postback URL if specified, that will load the
+     * specified object from the configured S3 source bucket.
+     *
+     * @param key
+     *            the key of the S3 object to be loaded from the configured source bucket as the source image
+     * @return a job builder with application ID and optionally postback URL set
+     */
+    public BlitlineImageJob.Builder loadS3Key(String key) {
+        return jobBuilder().fromS3(s3bucket, key);
+    }
+
+    /**
+     * Creates a job builder with the application ID set, along with the completion postback URL if specified, that will load the
+     * specified object from S3.
+     *
+     * @param bucket
+     *            the S3 bucket to load the source image from
+     * @param key
+     *            the key of the S3 object to be loaded from the specified source bucket as the source image
+     * @return a job builder with application ID and optionally postback URL set
+     */
+    public BlitlineImageJob.Builder loadS3Object(String bucket, String key) {
+        return jobBuilder().fromS3(bucket, key);
+    }
 }
